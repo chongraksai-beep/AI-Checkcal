a27dbd62a2f863c5cd39545e64905ca34c55710b
chongraksai-beep <chongrak.sai@gmail.com>
2026-09-13 00:36:32 +0700

    Update webhook002.js

--- api/webhook.js
+++ api/webhook.js
@@ -61,8 +61,8 @@ async function getLineImage(messageId) {
 
 // ฟังก์ชันส่งรูปให้ Gemini วิเคราะห์
 async function analyzeFoodWithGemini(imageBuffer) {
-  // ของใหม่ (เปลี่ยนเป็นรุ่น pro)
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-002" });
    
   // สั่ง AI ให้ตอบกลับมาเป็น JSON เพื่อง่ายต่อการเอาไปจัดลง Flex Message
   const prompt = `
